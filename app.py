053ba4b2c0b3eb6401b51a342b3235dc7bb1715a
Charinpat Pukhajeetrakul <charinpat.p@gmail.com>
2021-08-11 11:24:14 +0700

    Fix Bug

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import json
 import numpy as np
 import pandas as pd
 import geopy.distance as ps
-
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
 )
@@ -40,27 +39,6 @@ def callback():
         event_handle(event)
     return '',200
 
-def getdistace(latitude, longitude,cdat):
-  coords_1 = (float(latitude), float(longitude))
-  ## create list of all reference locations from a pandas DataFrame
-  latlngList = cdat[['Latitude','Longitude']].values
-  ## loop and calculate distance in KM using geopy.distance library and append to distance list
-  kmsumList = []
-  for latlng in latlngList:
-    coords_2 = (float(latlng[0]),float(latlng[1]))
-    kmsumList.append(ps.vincenty(coords_1, coords_2).km)
-  return kmsumList
-
-def handle_location(lat,lng,cdat,topK):
-    result = getdistace(lat, lng,cdat)
-    result = result.sort_values(by='km')
-    result = result.iloc[0:topK]
-    txtResult = ''
-    for i in range(len(result)):
-        kmdistance = '%.1f'%(result.iloc[i]['km'])
-        newssource = str(result.iloc[i]['News_Soruce'])
-        txtResult = txtResult + kmdistance + '\n' + newssource + '\n\n'
-    return txtResult[0:-2]
 
 def event_handle(event):
     print(event)
@@ -85,13 +63,43 @@ def event_handle(event):
         line_bot_api.reply_message(rtoken, replyObj)
         return ''
 
+
     if msgType == "location":
         lat = event["message"]["latitude"]
         lng = event["message"]["longitude"]
         txtresult = handle_location(lat,lng,casedata,3)
-        replyObj = TextSendMessage(text=msg)
+        replyObj = TextSendMessage(text=txtresult)
+        line_bot_api.reply_message(rtoken, replyObj)
+    else:
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
         line_bot_api.reply_message(rtoken, replyObj)
     return ''
+
+def handle_location(lat,lng,cdat,topK):
+    result = getdistace(lat, lng,cdat)
+    result = result.sort_values(by='km')
+    result = result.iloc[0:topK]
+    txtResult = ''
+    for i in range(len(result)):
+        kmdistance = '%.1f'%(result.iloc[i]['km'])
+        newssource = str(result.iloc[i]['News_Soruce'])
+        txtResult = txtResult + 'ห่าง %s กิโลเมตร\n%s\n\n'%(kmdistance,newssource)
+    return txtResult[0:-2]
+
+
+def getdistace(latitude, longitude,cdat):
+  coords_1 = (float(latitude), float(longitude))
+  ## create list of all reference locations from a pandas DataFrame
+  latlngList = cdat[['Latitude','Longitude']].values
+  ## loop and calculate distance in KM using geopy.distance library and append to distance list
+  kmsumList = []
+  for latlng in latlngList:
+    coords_2 = (float(latlng[0]),float(latlng[1]))
+    kmsumList.append(ps.vincenty(coords_1, coords_2).km)
+  cdat['km'] = kmsumList
+  return cdat
+
 
 if __name__ == '__main__':
     app.run(debug=True)
